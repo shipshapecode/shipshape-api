eaa65b673cdf545175b59146cf2d6a63e7a8b807
Robert Wagner <rwwagner90@gmail.com>
2017-05-03 16:38:22 -0400

    http and https

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200', 'https://shipshape.io'
+    origins 'localhost:4200', 'http://shipshape.io', 'https://shipshape.io'
 
     resource '*',
       headers: :any,
