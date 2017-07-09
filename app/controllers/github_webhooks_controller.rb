40459921a034749d6736913fdabf65090695329c
Robert Wagner <rwwagner90@gmail.com>
2017-07-09 17:50:13 -0400

    :joy:

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,8 +1,8 @@
 class GithubWebhooksController < ActionController::Base
   include GithubWebhook::Processor
   def github_watch(payload)
-    name = payload['repository.name']
-    stars = payload['repository.stargazers_count']
+    name = payload['repository']['name']
+    stars = payload['repository']['stargazers_count']
     puts name
     puts stars
     GithubRepo.where(name: name).first_or_create(name: name, stars: stars)
