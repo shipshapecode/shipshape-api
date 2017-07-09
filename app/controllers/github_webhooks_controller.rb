77c2b9483ac0f61a30ad5d979fac437549528839
Robert Wagner <rwwagner90@gmail.com>
2017-07-09 15:41:29 -0400

    Setup webhook secret

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -4,4 +4,8 @@ class GithubWebhooksController < ApplicationController
     name, stargazers_count = payload.repository
     GithubRepo.find_or_create_by(name: name, stars: stargazers_count)
   end
+
+  def webhook_secret(payload)
+    ENV['GITHUB_WEBHOOK_SECRET']
+  end
 end
