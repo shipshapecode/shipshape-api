3d6067337a944776c962d6ea39f4b64471c81842
Robert Wagner <rwwagner90@gmail.com>
2018-02-25 10:35:34 -0500

    Revert "Only allow getting github_repos"
    This reverts commit 40f3e2ed12710f45cf8f9430d5e3bb2c8e4028a7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   jsonapi_resources :contacts, only: [:create, :update]
-  jsonapi_resources :github_repos, only: :get
+  jsonapi_resources :github_repos
   resource :github_webhooks, only: :create, defaults: { formats: :json }
 end
